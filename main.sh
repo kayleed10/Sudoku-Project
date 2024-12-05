79f0586f117b95e0b0c4a3f40c75506793a08032
Shxmpathy <christiankeough1@gmail.com>
2024-12-05 14:21:00 -0500

    added easy, medium, and hard buttons to the screen

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ import pygame, sys
 def main_menu_draw(screen):
   title_font = pygame.font.SysFont("artifaktelement", 40)
   game_mode_font = pygame.font.SysFont("artifaktelement", 25)
-  button_font = pygame.font.SysFont("stencil", 20)
+  button_font = pygame.font.SysFont("comicsansms", 20, bold=True) #lucidasansrockwellextra
 
   sudoku_image = pygame.image.load("sudoku_image.jpg")
   sudoku_image = pygame.transform.scale(sudoku_image, (500, 500))
@@ -20,13 +20,50 @@ def main_menu_draw(screen):
   screen.blit(title_surface, title_rectangle)
   screen.blit(title_text, title_rectangle)
 
-  game_mode_surface = pygame.Surface((230,40)) #For showing title
+  game_mode_surface = pygame.Surface((230,40)) #For showing "game mode select" text
   game_mode_surface.fill((255, 255, 255))
   game_mode_text = game_mode_font.render("Select Game Mode:", 0, (0,0,0))
   game_mode_rectangle = game_mode_surface.get_rect(
   center = (500 //2, 500 // 2 - 15))
   screen.blit(game_mode_surface, game_mode_rectangle)
   screen.blit(game_mode_text, game_mode_rectangle)
+
+  ## Difficulty buttons
+  easy_text = button_font.render("EASY", 0, (255,255,255))
+  medium_text = button_font.render("MEDIUM", 0, (255,255,255))
+  hard_text = button_font.render("HARD", 0, (255,255,255))
+
+  ## Button background
+  easy_surface = pygame.Surface((90, 40))
+  medium_surface = pygame.Surface((125,40))
+  hard_surface = pygame.Surface((90,40))
+
+  ## Putting easy button on screen
+  easy_surface.fill((92, 64, 51))
+  easy_surface.blit(easy_text, (17,5))
+
+  easy_rectangle = easy_surface.get_rect(
+    center = (500 // 2 - 150, 500 // 2 + 110))
+
+  screen.blit(easy_surface, easy_rectangle)
+
+  ##Putting medium button on screen
+  medium_surface.fill((92, 64, 51))
+  medium_surface.blit(medium_text, (17,5))
+
+  medium_rectangle = medium_surface.get_rect(
+    center = (500 // 2, 500 // 2 + 110))
+
+  screen.blit(medium_surface, medium_rectangle)
+
+  ##Putting hard button on screen
+  hard_surface.fill((92, 64, 51))
+  hard_surface.blit(hard_text, (17,5))
+
+  hard_rectangle = hard_surface.get_rect(
+    center = (500 // 2 + 150, 500 // 2 + 110))
+
+  screen.blit(hard_surface, hard_rectangle)
 
   while True:
     for event in pygame.event.get():
